57e0a2f61605f25869869cd1f40c7fb3e0dfd93d
nzweben <nzweben@votem.com>
2017-10-09 14:07:39 -0400

    changed

--- assets/rotate.js
+++ assets/rotate.js
@@ -6,7 +6,7 @@ function hexToRgba(hex,a) {
 }
 
 var inc = 0;
-var css_text;
+var css_text ="";
 var update;
 
 $(document).ready(function(){
@@ -16,10 +16,10 @@ $(document).ready(function(){
 function spinIntroBlock(){
 	inc += .05;
 	inc = inc % 360;
-	css_text ="linear-gradient("+ (-135-inc) +"deg, rgba(0,0,0,.1),rgba(0,0,0,.8)),"; //black
-	css_text += "linear-gradient("+(180+inc)+"deg, rgba(0,100,100,0),rgba(0,255,255,.2)),"; //green
-	css_text += "linear-gradient("+(45+inc)+"deg, rgba(100,0,0,0),rgba(255,0,0,.3)),"; //red
-	css_text += "linear-gradient("+(-45-inc)+"deg,"+hexToRgba("#4175fc",.98)+","+hexToRgba("#221dc1",.95)+"),"; //blue/purp
-	css_text += "url('assets/triangles.jpg')";
+	// css_text +="linear-gradient("+ (-135-inc) +"deg, rgba(0,0,0,.1),rgba(0,0,0,.8)),"; //black
+	css_text += "linear-gradient("+(180+inc)+"deg, rgba(100,100,0,0),rgba(255,255,0,.2)),"; //green
+	css_text += "linear-gradient("+(45+inc)+"deg, rgba(100,0,0,0),rgba(255,0,0,.2)),"; //red
+	css_text += "linear-gradient("+(-45-inc)+"deg,"+hexToRgba("#ffa100",.98)+","+hexToRgba("#ff9d00",.95)+")"; //blue/purp
+	// css_text += "url('assets/triangles.jpg')";
 	$(".introBlock").css("background-image",css_text);
 }
