bec950e40d74e4b113836344e09df9fe371bd1ba
nzweben <nzweben@votem.com>
2017-10-10 11:28:03 -0400

    changes color

--- assets/effects.js
+++ assets/effects.js
@@ -13,3 +13,30 @@ $('a[href^="#"]').on('click', function(event) {
         }, 500);
     }
 });
+
+
+$(document).ready(function(){
+	$(window).scroll(function() {
+	    var height = $(window).scrollTop();
+
+	    // console.log(height);
+	    if (height <= 420){
+	    	console.log('here');
+	    	$("#nzdawg").css("color", "#ff9d00");
+	    }
+	   if (height > 420 && height <=965){
+		   	console.log('here2');
+	   	 	$("#nzdawg").css("color", "#4175fc");
+	   }
+	if (height > 965 && height <1500){
+		console.log("here3");
+		$("#nzdawg").css("color", "#3ca365");
+	} 
+	if (height > 1500){
+		console.log("here3");
+		$("#nzdawg").css("color", "#ad0938");
+	} 
+	
+	 
+	});
+});
